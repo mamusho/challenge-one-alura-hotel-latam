8a6282949694237955ba63a4e8589dfd38134853
Raymundo Morales <mamusho@gmail.com>
2023-08-28 17:34:40 -0600

    Base del proyecto

---
+++ src/com/hotelAlura/main/HotelAluraMain.java
@@ -0,0 +1,11 @@
+package com.hotelAlura.main;
+
+import views.MenuPrincipal;
+
+public class HotelAluraMain {
+	public static void main(String[] args) {
+		MenuPrincipal frame = new MenuPrincipal();
+		frame.setVisible(true);
+		
+	}
+}
